4a1c50055616b6b6786813e485c1542871c2a5e4
xidhu <sidhu3612@gmail.com>
2021-05-05 13:38:48 +0530

    2

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,28 +1,38 @@
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:we_now/app/routes/app_pages.dart';
+import 'package:motion_widget/motion_widget.dart';
 import 'package:we_now/app/theme/app_theme.dart';
+import 'package:we_now/app/widgets/homeview_components.dart';
 
 class HomeController extends GetxController {
-  var isDrawerOpen = false.obs;
-  var menuAnimationController;
-  var isDarkModeOn = false.obs;
-  var theme = AppTheme.lightTheme().obs;
-  final appData = GetStorage();
+  var appData = GetStorage();
+  var menuAnimator = MotionExitConfigurations(
+          durationMs: 500,
+          displacement: 200,
+          orientation: MotionOrientation.RIGHT)
+      .obs;
+
+  var isDrawerOpen;
+  var size;
+  var components;
+  var theme;
+  var isDarkModeOn;
 
   @override
   void onInit() {
     appData.writeIfNull('isDarkModeEnable', false);
-    this.isDrawerOpen = false.obs;
-    this.isDarkModeOn = (appData.read('isDarkModeEnable') as bool).obs;
+    isDrawerOpen = false.obs;
+    isDarkModeOn = (appData.read('isDarkModeEnable') as bool).obs;
+    size = Get.size;
+    theme = isDarkModeOn ? AppTheme.darkTheme() : AppTheme.lightTheme();
+    components = HomeViewComponents(size: size, theme: theme);
     super.onInit();
   }
 
-  void switchTheme(bool setDartkModeEnable) {
-    appData.write('isDarkModeEnable', setDartkModeEnable);
-    this.isDarkModeOn = setDartkModeEnable.obs;
-    this.theme = setDartkModeEnable
+  void switchTheme(bool setDarkModeEnable) {
+    appData.write('isDarkModeEnable', setDarkModeEnable);
+    isDarkModeOn = setDarkModeEnable.obs;
+    theme = setDarkModeEnable
         ? AppTheme.darkTheme().obs
         : AppTheme.lightTheme().obs;
     update();
