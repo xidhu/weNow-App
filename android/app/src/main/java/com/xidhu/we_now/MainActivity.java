98f46c447a4cf8744e2a4999bb415cb3ea831aea
xidhu <sidhu3612@gmail.com>
2021-05-16 14:10:32 +0530

    init

---
+++ android/app/src/main/java/com/xidhu/we_now/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xidhu.we_now;
+
+import android.os.Bundle;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends io.flutter.app.FlutterActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:we_now/app/data/Api/weather_api.dart';
-import 'package:we_now/app/data/models/location_model.dart';
 import 'package:we_now/app/settings/settings.dart';
 import 'package:we_now/app/utils/local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
 import 'app/data/database/database.dart';
-import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'app/data/models/location_model.dart';
 import 'app/routes/app_pages.dart';
-import 'app/utils/connectivity.dart';
 
-backGroundTask(String locationId, bool isCelcius) async {
-  var connectionStatus = ConnectionStatusSingleton.getInstance();
-  connectionStatus.initialize();
-  if ((await connectionStatus.checkConnection())) {
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
     var database = AppDatabase.instance;
-    Location data = (await database.getLocation(locationId));
+    Location data = (await database.getLocation(inputData!['locId']));
     data.setFullWeather(
         await WeatherApi().getFullWeather(data.longitude, data.latitude));
-    LocalNotifications()
-        .showNotificationWithDefaultSound(location: data, isCelcius: isCelcius);
-  }
+
+    LocalNotifications().showNotificationWithDefaultSound(
+        location: data, isCelcius: inputData['isCel']);
+    return Future.value(true);
+  });
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Settings appSettings = Settings.instance;
   await appSettings.loadData();
-  await AndroidAlarmManager.initialize();
+
+  if (appSettings.currentLocation >= 0) {
+    Workmanager wrk = Workmanager();
+    wrk.initialize(callbackDispatcher);
+    wrk.registerPeriodicTask("1", "backgroundNotification",
+        existingWorkPolicy: ExistingWorkPolicy.replace,
+        constraints: Constraints(
+          networkType: NetworkType.connected,
+        ),
+        inputData: {
+          'locId': appSettings.currentLocationId,
+          'isCel': appSettings.isCelciuis
+        },
+        frequency: Duration(hours: 3),
+        initialDelay: Duration(seconds: 5));
+  }
+
   runApp(GetMaterialApp(
     theme: ThemeData(
       fontFamily: 'ReemKufi',
@@ -36,9 +51,4 @@ void main() async {
         appSettings.currentLocation >= 0 ? AppPages.INITIAL : AppPages.LOCATION,
     getPages: AppPages.routes,
   ));
-  await appSettings.loadData();
-  if (appSettings.currentLocation >= 0) {
-    await AndroidAlarmManager.periodic(const Duration(minutes: 1), 0,
-        backGroundTask(appSettings.currentLocationId, appSettings.isCelciuis));
-  }
 }
